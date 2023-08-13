f4fa3db6e39c27e284c70dad68f59cb345664adc
SumeyraGnl <h.berengunel@gmail.com>
2023-08-13 13:15:54 +0300

    sumeyra/13/08/v2

--- src/test/java/stepDefinitions/APIStepDefinition.java
+++ src/test/java/stepDefinitions/APIStepDefinition.java
@@ -11,6 +11,7 @@ import io.restassured.response.Response;
 import org.hamcrest.Matchers;
 import org.json.JSONObject;
 import org.junit.Assert;
+import testData.TestData_US_033;
 import utilities.ApiUtils;
 import utilities.Authentication;
 import utilities.ConfigReader;
@@ -19,6 +20,7 @@ import java.util.HashMap;
 
 import static hooks.api.HooksAPI.spec;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.hasItem;
 
 public class APIStepDefinition {
 
@@ -88,5 +90,49 @@ public class APIStepDefinition {
 
         Assert.assertEquals(expectedData.get("status"),ApiUtils.respHP.get("status"));
         Assert.assertEquals(expectedData.get("message"),ApiUtils.respHP.get("message"));
+    }
+
+
+    @Given("Response body content test for visitor list")
+    public void response_body_content_test_for_visitor_list() {
+
+        TestData_US_033 testDataUs033 = new TestData_US_033();
+
+        JSONObject expData = testDataUs033.expDataUS_033();
+
+        JsonPath resJP = response.jsonPath();
+
+        Assert.assertEquals(expData.getJSONObject("data").get("id"),resJP.getJsonObject("lists.id"));
+        Assert.assertEquals(expData.getJSONObject("data").get("staff_id"),resJP.getJsonObject("lists.staff_id"));
+        Assert.assertEquals(expData.getJSONObject("data").get("student_session_id"),resJP.getJsonObject("lists.student_session_id"));
+        Assert.assertEquals(expData.getJSONObject("data").get("source"),resJP.getJsonObject("lists.source"));
+        Assert.assertEquals(expData.getJSONObject("data").get("purpose"),resJP.getJsonObject("lists.purpose"));
+        Assert.assertEquals(expData.getJSONObject("data").get("name"),resJP.getJsonObject("lists.name"));
+        Assert.assertEquals(expData.getJSONObject("data").get("email"),resJP.getJsonObject("lists.email"));
+        Assert.assertEquals(expData.getJSONObject("data").get("contact"),resJP.getJsonObject("lists.contact"));
+        Assert.assertEquals(expData.getJSONObject("data").get("id_proof"),resJP.getJsonObject("lists.id_proof"));
+        Assert.assertEquals(expData.getJSONObject("data").get("no_of_people"),resJP.getJsonObject("lists.no_of_people"));
+        Assert.assertEquals(expData.getJSONObject("data").get("date"),resJP.getJsonObject("lists.date"));
+        Assert.assertEquals(expData.getJSONObject("data").get("in_time"),resJP.getJsonObject("lists.in_time"));
+        Assert.assertEquals(expData.getJSONObject("data").get("out_time"),resJP.getJsonObject("lists.out_time"));
+        Assert.assertEquals(expData.getJSONObject("data").get("note"),resJP.getJsonObject("lists.note"));
+        Assert.assertEquals(expData.getJSONObject("data").get("image"),resJP.getJsonObject("lists.image"));
+        Assert.assertEquals(expData.getJSONObject("data").get("meeting_with"),resJP.getJsonObject("lists.meeting_with"));
+        Assert.assertEquals(expData.getJSONObject("data").get("created_at"),resJP.getJsonObject("lists.created_at"));
+        Assert.assertEquals(expData.getJSONObject("data").get("class"),resJP.getJsonObject("lists.class"));
+        Assert.assertEquals(expData.getJSONObject("data").get("section"),resJP.getJsonObject("lists.section"));
+        Assert.assertEquals(expData.getJSONObject("data").get("staff_name"),resJP.getJsonObject("lists.staff_name"));
+        Assert.assertEquals(expData.getJSONObject("data").get("staff_surname"),resJP.getJsonObject("lists.staff_surname"));
+        Assert.assertEquals(expData.getJSONObject("data").get("staff_employee_id"),resJP.getJsonObject("lists.staff_employee_id"));
+        Assert.assertEquals(expData.getJSONObject("data").get("class_id"),resJP.getJsonObject("lists.class_id"));
+        Assert.assertEquals(expData.getJSONObject("data").get("section_id"),resJP.getJsonObject("lists.section_id"));
+        Assert.assertEquals(expData.getJSONObject("data").get("students_id"),resJP.getJsonObject("lists.students_id"));
+        Assert.assertEquals(expData.getJSONObject("data").get("admission_no"),resJP.getJsonObject("lists.admission_no"));
+        Assert.assertEquals(expData.getJSONObject("data").get("student_firstname"),resJP.getJsonObject("lists.student_firstname"));
+        Assert.assertEquals(expData.getJSONObject("data").get("student_middlename"),resJP.getJsonObject("lists.student_middlename"));
+        Assert.assertEquals(expData.getJSONObject("data").get("student_lastname"),resJP.getJsonObject("lists.student_lastname"));
+        Assert.assertEquals(expData.getJSONObject("data").get("role_id"),resJP.getJsonObject("lists.role_id"));
+
+
     }
 }
--- src/test/resources/features/API/[API_US033].feature
+++ src/test/resources/features/API/[API_US033].feature
@@ -1,5 +1,24 @@
-Feature: As an administrator, I want to access the Visitor List through API connection.
-
-  Scenario: Tests that the response returning valid autorization information is successful
 
 
+Feature: API_US001 As an administrator, I want to access the Visitor List through API connection.
+
+
+  Scenario: TC_001 GET request success response with valid information for visitor list
+
+    * "ADMIN" Page User sets "api/visitorsList" parameters
+    * User sends a get request
+    * Execute a verification for a valid authorization
+
+
+  Scenario: TC_002 GET request failed response with invalid information for visitor list
+
+    * "INVALID" Page User sets "api/visitorsList" parameters
+    * User sends a get request
+    * Execute a verification for an invalid authorization
+
+  @API
+  Scenario: TC_003 Validates information of data with id=250 in response body content
+
+    * "ADMIN" Page User sets "api/visitorsList" parameters
+    * User sends a get request
+    * Response body content test for visitor list
